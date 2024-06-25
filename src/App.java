f75a0e4dc776110672455e1a16676977dad30955
MangalHemant <hemantmangal22@gmail.com>
2024-06-25 21:11:06 +0530

    Testing

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+        MyMap<Integer,String> map=new MyMap<>();
+        map.put(1,"Hemant");
+        map.put(2,"Janvi");
+        map.put(3,"Gourav");
+        map.put(4,"Akshya");
+        map.put(5,"Ankit");
+        map.put(6,"Ankita");
+        map.put(7,"Jai");
+        map.put(8,"Hema");
+        System.out.println(map.get(2));
+        map.remove(2);
+        System.out.println(map.get(2));
+        System.out.println(map.get(7));
+        map.put(2,"Janvi");
+        System.out.println(map.get(2));
+        }
 }
